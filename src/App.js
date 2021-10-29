d8818357074ec96d766abe1197dba2089dc23e1e
tainguyentt <tai.nguyen@zalora.com>
2021-10-29 15:47:21 +0700

    Add logout feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import OpenLogin from '@toruslabs/openlogin';
+
+const VERIFIER = {
+    loginProvider: "google",
+    clientId: "PROJECT ID"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLoading, setLoading] = useState(true);
+    const [openlogin, setOpenLogin] = useState();
+    const [privKey, setPrivKey] = useState();
+
+    const onMount = async () => {
+        setLoading(true);
+
+        try {
+            const openlogin = new OpenLogin({
+                clientId: VERIFIER.clientId,
+                network: "testnet"
+            });
+            setOpenLogin(openlogin); //setState???
+
+            await openlogin.init();
+            setPrivKey(openlogin.privKey);
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const onLogin = async () => {
+        if (isLoading || privKey) return;
+
+        setLoading(true);
+        try {
+            //trigger login
+            await openlogin.login({
+                loginProvider: VERIFIER.loginProvider,
+                redirectUrl: "http://localhost:3000/redirect",
+            });
+            setPrivKey(openlogin.privKey); //setState???
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const onLogout = async () => {
+        await openlogin.logout();
+        setPrivKey('');
+    }
+
+    useEffect(() => {
+        onMount();
+    }, []);
+
+    if (isLoading) return <div>Loading...</div>
+    return privKey ?
+        (<div>
+            Logged in: {privKey}
+            <button onClick={onLogout}>Logout</button>
+        </div>) :
+        (<button onClick={onLogin}>Login</button>);
 }
 
 export default App;
