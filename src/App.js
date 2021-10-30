7267ce32f5db5a5763b5895c4acb9c73f7f9ba2d
tainguyentt <tai.nguyen@zalora.com>
2021-10-30 11:33:32 +0700

    Add more user info

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     const [privKey, setPrivKey] = useState();
     const [address, setAddress] = useState();
     const [balance, setBalance] = useState();
+    const [userInfo, setUserInfo] = useState();
 
     useEffect(() => {
         async function onMount() {
@@ -22,14 +23,17 @@ function App() {
             try {
                 const openlogin = new OpenLogin({
                     clientId: VERIFIER.clientId,
-                    network: "testnet"
+                    network: "testnet",
+                    redirectUrl: "http://localhost:3000/redirect",
+                    uxMode: "popup"
                 });
                 setOpenLogin(openlogin);
 
+                //sync up the state of user login session, add privKey if user logged in before
+                //inject an iframe to DOM
                 await openlogin.init();
 
-                await connectEther(openlogin.privKey);
-                setPrivKey(openlogin.privKey);
+                await connectEther(openlogin);
             } finally {
                 setLoading(false);
             }
@@ -43,25 +47,24 @@ function App() {
         setLoading(true);
         try {
             //trigger login
-            await openlogin.login({
-                loginProvider: VERIFIER.loginProvider,
-                redirectUrl: "http://localhost:3000/redirect",
-            });
-            setPrivKey(openlogin.privKey);
+            await openlogin.login();
 
-            await connectEther(openlogin.privKey);
+            await connectEther(openlogin);
         } finally {
             setLoading(false);
         }
     }
 
-    async function connectEther(privKey) {
-        if (!privKey) {
+    async function connectEther(openlogin) {
+        if (!openlogin.privKey) {
             return;
         }
-        const wallet = new ethers.Wallet(privKey, ethers.getDefaultProvider());
+        const wallet = new ethers.Wallet(openlogin.privKey, ethers.getDefaultProvider());
         const address = await wallet.getAddress();
         const balance = await wallet.getBalance();
+        const userInfo = await openlogin.getUserInfo();
+        setPrivKey(openlogin.privKey);
+        setUserInfo(userInfo);
         setAddress(address);
         setBalance(balance.toString());
     }
@@ -76,8 +79,8 @@ function App() {
         <div class="container">
             <div class="header clearfix"> <nav>
                 <ul class="nav nav-pills pull-right">
-                    <li role="presentation" class="active"><a href="#">Home</a></li>
-                    <li role="presentation">{privKey ? <a role="button" onClick={onLogout}>Log-out</a> : <span></span>}</li>
+                    <li role="presentation" class="active"><a href="/#">Home</a></li>
+                    <li role="presentation">{privKey ? <a role="button" href="/#" onClick={onLogout}>Log-out</a> : <span></span>}</li>
                 </ul>
             </nav>
                 <h3 class="text-muted">Torus Demo</h3>
@@ -88,11 +91,17 @@ function App() {
                 <p>
                     {isLoading ? <div>Loading...</div> :
                         privKey ? (<div>
+                            <p><strong>Verifier:</strong> {userInfo.verifier}</p>
+                            <p><strong>Aggregate Verifier:</strong> {userInfo.aggregateVerifier}</p>
+                            <p><strong>Verifier Id:</strong> {userInfo.verifierId}</p>
+                            <p><strong>Type of Login:</strong> {userInfo.typeOfLogin}</p>
+                            <p><strong>Email:</strong> {userInfo.email}</p>
                             <p><b>Address:</b> {address}</p>
-                            <p><b>Balance:</b> {balance}</p></div>)
+                            <p><b>Balance:</b> {balance}</p>
+                            </div>)
                             : (<p class="lead">
                                 <p>A plug n play auth suite that combines the simplicity of passwordless authentication with the security of non-custodial public key infrastructure.</p>
-                                <a class="btn btn-lg btn-success" href="#" role="button" onClick={onLogin}>Login</a>
+                                <a class="btn btn-lg btn-success" href="/#" role="button" onClick={onLogin}>Login</a>
                             </p>)
                     }
                 </p>
